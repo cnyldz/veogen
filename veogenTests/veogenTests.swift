0d490e68312624665551912c1ccfd17a2499ae71
heavyshark <cnyldz@gmail.com>
2025-06-06 13:17:05 +0300

    Initial Commit

---
+++ veogenTests/veogenTests.swift
@@ -0,0 +1,17 @@
+//
+//  veogenTests.swift
+//  veogenTests
+//
+//  Created by Heavyshark on 6.06.2025.
+//
+
+import Testing
+@testable import veogen
+
+struct veogenTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
